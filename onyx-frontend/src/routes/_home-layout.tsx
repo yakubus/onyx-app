6ea147abcb64f49a5e28612d9913ea9b27354b49
fangel42 <frank.aniolkowski42@gmail.com>
2024-06-02 16:34:37 +0200

    Login Dialog

--- onyx-frontend/src/routes/_home-layout.tsx
+++ onyx-frontend/src/routes/_home-layout.tsx
@@ -4,12 +4,14 @@ import { createFileRoute } from "@tanstack/react-router";
 
 import Hero from "@/components/home/Hero";
 import Navbar from "@/components/home/Navbar";
+import Login from "@/components/home/Login";
 
 export const Route = createFileRoute("/_home-layout")({
   component: () => (
     <div className="max-w-1440px md:w-full lg:max-w-1440px xl:w-1440px max-h-700px h-auto mx-auto overflow-hidden bg-background">
       <Navbar/>
       <Hero />
+      <Login/>
     </div>
   ),
 });
