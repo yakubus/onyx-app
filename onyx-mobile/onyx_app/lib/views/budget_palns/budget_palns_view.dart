efe065cfa3c9ab7f3254b34b5bc9094baed3e77a
Yakubus <yaqbg1@gmail.com>
2024-07-02 23:51:45 +0200

    zmiana sposobu listowania budżetów + dialog add budget

--- onyx-mobile/onyx_app/lib/views/budget_palns/budget_palns_view.dart
+++ onyx-mobile/onyx_app/lib/views/budget_palns/budget_palns_view.dart
@@ -1,22 +1,17 @@
-import 'dart:async';
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:onyx_app/main.dart';
-import 'package:onyx_app/services/budget/budget.dart';
+import 'package:onyx_app/views/budget_palns/add_budget.dart';
 import 'package:onyx_app/views/budget_palns/budget_list.dart';
 import 'package:onyx_app/widgets/appbar.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class BudgetPlansView extends HookConsumerWidget {
   const BudgetPlansView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final budgetData = ref.watch(budgetServiceDataProvider);
-
     return Scaffold(
       appBar: DefaultAppBar(
         title: AppLocalizations.of(context)!.budget_plans,
@@ -30,9 +25,25 @@ class BudgetPlansView extends HookConsumerWidget {
             Text(AppLocalizations.of(context)!.budget_view_title,
                 style: GoogleFonts.lato(fontSize: 30),
                 textAlign: TextAlign.center),
+            const SizedBox(height: 20),
             Text(AppLocalizations.of(context)!.budget_view_title_desctiption,
                 textAlign: TextAlign.center),
-            BudgetList(),
+            const SizedBox(height: 50),
+            const BudgetList(),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: ShadButton.outline(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return const AddBudgetDialog();
+                    },
+                  );
+                },
+                icon: const Icon(Icons.add),
+              ),
+            )
           ],
         ),
       ),
