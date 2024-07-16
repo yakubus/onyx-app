194a6c333597fc6f9aba7a4c8e1a6a7294eea5b0
Maciej Mądry <maciejmadry0@gmail.com>
2024-07-16 14:32:02 +0200

    FIX: currency list

--- onyx-frontend/src/lib/constants/currency.ts
+++ onyx-frontend/src/lib/constants/currency.ts
@@ -2,6 +2,5 @@ export const CURRENCY = [
   { label: "PLN", value: "PLN" },
   { label: "USD", value: "USD" },
   { label: "EUR", value: "EUR" },
-  { label: "GBP", value: "GBP" },
   { label: "CHF", value: "CHF" },
 ] as const;
